393cbb8fc28f9cc5e63e24ca615a7b9e553c128a
Asraa Ayoub <asraaayoub@gmail.com>
2022-01-23 11:11:03 +0200

    text

--- database/init.sql
+++ database/init.sql
@@ -4,25 +4,26 @@ DROP TABLE IF EXISTS users, blog_posts CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  age INTEGER,
+  address VARCHAR(255) NOT NULL,
+  size INTEGER,
+  rooms INTEGER,
+  price INTEGER,
+  price_range INTEGER,
   location VARCHAR(255)
 );
 
 CREATE TABLE blog_posts (
   id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(id),
-  text_content TEXT
+  review_content TEXT
 );
-INSERT INTO users (username, age, location) VALUES
-  ('Sery1976', 28, 'Middlehill, UK'),
-  ('Notne1991', 36, 'Sunipol, UK'),
-  ('Moull1990', 41, 'Wanlip, UK'),
-  ('Spont1935', 72, 'Saxilby, UK'),
-  ('Precand', 19, 'Stanton, UK')
+
+        
+INSERT INTO posts (contact , address, size, rooms, price,price_range, photos) VALUES
+  ('
 ;
 
-INSERT INTO blog_posts (text_content, user_id) VALUES
+INSERT INTO blog_posts (review_content, user_id) VALUES
   ('Announcing of invitation principles in.', 1),
   ('Peculiar trifling absolute and wandered yet.', 2),
   ('Far stairs now coming bed oppose hunted become his.', 3),
--- router.js
+++ router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const posts = require("./handlers/posts");
-
-
+const home = require(" ./handlers/home");
+const login = require(" ./handlers/login");
+const new_post = require(" ./handlers/new_post");
+const signup= require(" ./handlers/signup");
 // router.get('/', handlers.home)
 // router.get('/new-user', handlers.newUser)
 // router.post('/create-user', handlers.createUser)
@@ -11,5 +13,7 @@ router.get('/posts',posts.posts)
 router.use((req, res) => {
   res.status(404).send(`<h1>Not found</h1>`)
 })
+ //return json 
+
 
 module.exports = router;
