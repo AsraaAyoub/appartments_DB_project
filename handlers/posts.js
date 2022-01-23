3215dca3d7d6b7abc7115390b8b1e9e004967950
“Julian” <j.makhoul1995@gmail.com>
2022-01-23 11:16:06 +0200

    Update posts.js

--- handlers/posts.js
+++ handlers/posts.js
@@ -1,12 +1,13 @@
-//all posts function 
-//posts function return html 
+//all posts function
+//posts function return html
 function allPosts(req, res) {
-  db.query("SELECT username FROM users").then(result => {
+  db.query("SELECT username FROM users").then((result) => {
     const users = result.rows;
-  })
+  });
 }
 function posts(req, res) {
-  db.query("SELECT username FROM users").then(result => {
+  db.query("SELECT username FROM users").then((result) => {
     const users = result.rows;
-  })
+  });
+  console.log(users);
 }
